438a89264b0319b092c28051e8e1fe3341ec9e4a
unknown <shakthivelmanjeedan11@gmail.com>
2024-12-06 22:32:48 +0530

    Re-implemented categories logics to a new catergory application file

--- eCommerce-project/eCommerce-backend/src/applications/product.js
+++ eCommerce-project/eCommerce-backend/src/applications/product.js
@@ -55,15 +55,6 @@ const products = [
       },
 ];
 
-const categories = [
-  { id: "1", name: "Headphones" },
-  { id: "2", name: "Earbuds" },
-  { id: "3", name: "Smart Speakers" },
-  { id: "4", name: "Smartwatches" },
-];
-
-//-------------------Product logics---------------------------
-
 export const getProducts = (req, res) => {
     return res.status(200).json(products).send();
 }
@@ -109,51 +100,3 @@ export const updateProductById = (req,res) => {
 
     return res.status(200).send(`Product info at ${productIndex} updated successfully`);
 }
-
-// --------------------Category logics---------------------------
-
-export const getCategories = (req,res) => {
-    return res.status(200).json(categories).send();
-}
-
-export const createCategory = (res,req) => {
-    categories.push(req.body);
-    return res.status(200).send("Category added successfully");
-}
-
-export const getCategoriesById = (req,res) => {
-    const id = req.params.id;
-    const categoryIndex = categories.find((pro) => {
-        pro.id === id;
-    })
-    return res.status(200).json(category).send();
-}
-
-export const deleteCategoriesById = (req,res) => {
-    const id = req.params.id;
-    const categoryIndex = categories.find((pro) => {
-        pro.id === id;
-    })
-
-    if(categoryIndex !== -1){
-        categories.splice(categoryIndex, 1);
-    }
-
-    return res.status(200).send(`Category field data at ${categoryIndex} id removed successfully`);
-}
-
-export const updateCategoryById = (req,res) => {
-    const id = req.params.id;
-    const categoryIndex = categories.find((pro) => {
-        pro.id === id;
-    })
-
-    if(categoryIndex !== -1){
-        categoryIndex.name = req.body.name;
-    }
-
-    return res.status(200).send(`Category field data at ${categoryIndex} id is updated successfully`);
-}
-
-
-
