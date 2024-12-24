e3034dd924a090127f719b800f1c2c49573f71a3
unknown <shakthivelmanjeedan11@gmail.com>
2024-12-24 10:36:24 +0530

    Modified the styling of ProductCard.jsx

--- pulzo-eCommerce-project/pulzo-frontend/src/ProductCard.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/ProductCard.jsx
@@ -1,30 +1,32 @@
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Heart } from "lucide-react";
 import { useState } from "react";
 
 function ProductCard(props) {
-  const [num, setNum] = useState(0);
-
   const handleClick = () => {
-    setNum(num + 1);
+    console.log("Add to Cart {test:ok}");
   };
 
   return (
-    <Card>
-      <div className="h-80 bg-card rounded-lg p-4 relative">
-        <img src={props.image} className="block" />
+    <Card className="border-none ">
+      <div className="bg-gray-50 rounded-lg p-4 flex justify-center items-center relative">
+        <img src={props.image} className="w-full h-full object-cover" />
+        <Heart className="absolute top-4 right-4 cursor-pointer z-10 text-red-500" />
       </div>
-      <div className="flex px-4 mt-4  items-center justify-between">
-        <h2 className="text-2x1  font-semibold">{props.name}</h2>
-        <span className="block text-lg font-medium">{props.price}</span>
-        <p>{num}</p>
+      <div className="flex items-center justify-between">
+        <span className="block text-2xl font-semibold">{props.name}</span>
+        <span className="block font-semibold">${props.price}</span>
       </div>
-      <div className="px-4 mt-2">
+      <div className="text-sm">
         <p className="text-sm">{props.description}</p>
       </div>
-      <div className="mt-1 p-4">
-        <Button className="w-full" onClick={handleClick}>
-          Buy now
+      <div className="mt-2">
+        <Button
+          className=" bg-white border-2 border-black text-black px-4 py-1 text-lg rounded-lg mt-2 font-medium hover:bg-black hover:text-white transition duration-200 ease-in-out"
+          onClick={handleClick}
+        >
+          Add to Cart
         </Button>
       </div>
     </Card>
