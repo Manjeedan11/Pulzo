02ade9c9a4d6d43de857b46f4b149089d0d24df2
unknown <shakthivelmanjeedan11@gmail.com>
2024-12-24 22:59:39 +0530

    Implemented and initialized CORS policy

--- pulzo-eCommerce-project/pulzo-backend/src/app.js
+++ pulzo-eCommerce-project/pulzo-backend/src/app.js
@@ -5,9 +5,11 @@ import { categoriesRouter } from "./api/category.js";
 import { userRouter } from "./api/user.js";
 import globalErrorHandlingMiddleware from "./api/middleware/global-error-handling-middleware.js";
 import { connectDB } from "./infrastructure/db.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api/products", productRouter);
 app.use("/api/categories", categoriesRouter);
