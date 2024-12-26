d1d023993e8d19c7016c59a019eaf74ae163c1f0
unknown <shakthivelmanjeedan11@gmail.com>
2024-12-26 21:33:21 +0530

    Added useState logic for handling Cart functionality with use of static product array

--- pulzo-eCommerce-project/pulzo-frontend/src/pages/HomePage.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/pages/HomePage.jsx
@@ -4,10 +4,31 @@ import Products from "@/Products";
 import { useState } from "react";
 
 function HomePage() {
-  const name = "Manjeedan";
+  const name = null;
   const [cart, setCart] = useState([]);
 
-  const handleAddToCart = (product) => {};
+  const handleAddToCart = (product) => {
+    const foundItem = cart.find((item) => item.product._id === product._id);
+    if (foundItem) {
+      setCart(
+        cart.map((cartItem) => {
+          cartItem.product._id === product._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem;
+        })
+      );
+      return;
+    }
+    setCart([...cart, { product: product, quantity: 1 }]);
+  };
+
+  const getCartQuantity = () => {
+    let count = 0;
+    cart.forEach((item) => {
+      count += item.quantity;
+    });
+    return count;
+  };
 
   return (
     <div>
