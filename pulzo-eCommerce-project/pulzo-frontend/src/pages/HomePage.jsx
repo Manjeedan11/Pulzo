b4e0b3cd054b8f13c18445eb0bddfc947d4cbc93
unknown <shakthivelmanjeedan11@gmail.com>
2025-01-01 19:42:16 +0530

    Re-committing Home.jsx with the same code

--- pulzo-eCommerce-project/pulzo-frontend/src/pages/HomePage.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
 import NavBar from "@/NavBar";
 import Hero from "@/Hero";
 import Products from "@/Products";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function HomePage() {
   const name = "Manjeedan";
-  const cartCount = 0;
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) => {};
 
   return (
     <div>
-      <NavBar name={name} cartCount={cartCount} />
+      <NavBar
+        name={name}
+        cartCount={getCartQuantity()}
+        favoritesCount={favorite.length}
+      />
       <Hero />
       <Products />
     </div>
