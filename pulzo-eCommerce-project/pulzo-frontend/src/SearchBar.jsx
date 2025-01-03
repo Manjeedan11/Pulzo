4a1565cec21a29fc69ba71fe3e9594024d9bc4f1
unknown <shakthivelmanjeedan11@gmail.com>
2025-01-03 17:24:31 +0530

    Modified the styling of SearchBar component

--- pulzo-eCommerce-project/pulzo-frontend/src/SearchBar.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/SearchBar.jsx
@@ -5,8 +5,8 @@ import { Search } from "lucide-react";
 function SearchBar() {
   return (
     <div className="relative">
-      <Input className="w-100 pr-12" placeholder="Search" />
-      <Button className="absolute right-0 top-0 h-full bg-gray-400">
+      <Input className="w-100 pr-12 rounded-full" placeholder="Search" />
+      <Button className="absolute right-0 top-0 h-full bg-gray-400 rounded-full">
         <Search />
       </Button>
     </div>
