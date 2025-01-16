a9aa8d89aeee60680d3aaa6bc2205b7c7faf18b7
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-01-16 22:52:52 +0530

    installed clerk's dependencies and initialized boilerplate code

--- pulzo-eCommerce-project/pulzo-frontend/src/main.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/main.jsx
@@ -10,17 +10,26 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 
 import { store } from "@/lib/store.js";
 import { Provider } from "react-redux";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
   //<StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
+  <ClerkProvider publishableKey="{PUBLISHABLE_KEY}" afterSignOutUrl="/">
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </ClerkProvider>
   //</StrictMode>
 );
