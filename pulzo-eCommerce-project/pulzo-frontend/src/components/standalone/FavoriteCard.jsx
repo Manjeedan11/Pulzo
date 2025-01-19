60aabaf5975f2324513852ad7cfca03e91f63a6c
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-01-19 13:23:16 +0530

    refactored the code for proper flex layout styling and it's implemented

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/FavoriteCard.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/FavoriteCard.jsx
@@ -4,40 +4,47 @@ import { Heart } from "lucide-react";
 import { addToCart } from "@/lib/features/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 
-function FavoriteCard(props) {
+function FavoriteCard() {
   const dispatch = useDispatch();
   const favoritesCard = useSelector((state) => state.favorite.value);
 
-  const handleClick = (e) => {
+  const handleClick = (item) => {
     dispatch(
       addToCart({
-        _id: props._id,
-        name: props.name,
-        price: props.price,
-        image: props.image,
-        description: props.description,
+        _id: item._id,
+        name: item.name,
+        price: item.price,
+        image: item.image,
+        description: item.description,
       })
     );
   };
 
   return (
-    <div className="flex gap-5 overflow-x-auto">
+    <div className="flex flex-wrap gap-6">
       {favoritesCard.map((item) => (
-        <Card className="border-none w-64">
+        <Card key={item._id} className="w-[300px] border-none">
           <div className="bg-gray-50 rounded-lg p-4 flex justify-center items-center relative">
-            <img src={item.image} className="w-full h-full object-cover" />
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-[200px] object-cover rounded-lg"
+            />
+            <div className="absolute top-4 right-4 cursor-pointer z-10 text-red-500">
+              <Heart fill="none" className="w-6 h-6" />
+            </div>
           </div>
-          <div className="flex items-center justify-between">
-            <span className="block text-2xl font-semibold">{item.name}</span>
-            <span className="block font-semibold">${item.price}</span>
+          <div className="mt-4">
+            <div className="flex items-center justify-between">
+              <span className="block text-lg font-semibold">{item.name}</span>
+              <span className="block font-semibold">${item.price}</span>
+            </div>
+            <p className="text-sm mt-2">{item.description}</p>
           </div>
-          <div className="text-sm">
-            <p className="text-sm">{item.description}</p>
-          </div>
-          <div className="mt-2">
+          <div className="mt-4">
             <Button
-              className="bg-white border-2 border-black text-black px-4 py-1 text-lg rounded-lg mt-2 font-medium hover:bg-black hover:text-white transition duration-200 ease-in-out"
-              onClick={handleClick}
+              className="bg-white border-2 border-black text-black px-4 py-1 text-lg rounded-lg font-medium hover:bg-black hover:text-white transition"
+              onClick={() => handleClick(item)}
             >
               Add to Cart
             </Button>
--- pulzo-eCommerce-project/pulzo-frontend/src/pages/FavoritePage.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/pages/FavoritePage.jsx
@@ -4,7 +4,7 @@ function FavoritePage() {
   return (
     <section className="py-8 px-4 xl:px-16">
       <h2 className="text-4xl font-bold">My Favorites</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+      <div className="flex flex-wrap gap-6 mt-5">
         <FavoriteCard />
       </div>
     </section>
