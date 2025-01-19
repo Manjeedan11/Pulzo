72c76951085e81e1d9ec4c3e570737dab71b8686
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-01-19 13:41:08 +0530

    refactored the code to implement removeFavorites logic to the component

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/FavoriteCard.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/FavoriteCard.jsx
@@ -27,35 +27,44 @@ function FavoriteCard() {
 
   return (
     <div className="flex flex-wrap gap-6">
-      {favoritesCard.map((item) => (
-        <Card key={item._id} className="w-[300px] border-none">
-          <div className="bg-gray-50 rounded-lg p-4 flex justify-center items-center relative">
-            <img
-              src={item.image}
-              alt={item.name}
-              className="w-full h-[200px] object-cover rounded-lg"
-            />
-            <div className="absolute top-4 right-4 cursor-pointer z-10 text-red-500">
-              <Heart fill="none" className="w-6 h-6" />
+      {favoritesCard.length === 0 ? (
+        <p className="text-center w-full text-xl font-semibold">
+          No favorites added
+        </p>
+      ) : (
+        favoritesCard.map((item) => (
+          <Card key={item._id} className="w-[300px] border-none">
+            <div className="bg-gray-50 rounded-lg p-4 flex justify-center items-center relative">
+              <img
+                src={item.image}
+                alt={item.name}
+                className="w-full h-[200px] object-cover rounded-lg"
+              />
+              <div
+                className="absolute top-4 right-4 cursor-pointer z-10 text-red-500"
+                onClick={() => toggleFavoriteHandler(item)}
+              >
+                <Heart fill="red" className="w-6 h-6" />
+              </div>
             </div>
-          </div>
-          <div className="mt-4">
-            <div className="flex items-center justify-between">
-              <span className="block text-lg font-semibold">{item.name}</span>
-              <span className="block font-semibold">${item.price}</span>
+            <div className="mt-4">
+              <div className="flex items-center justify-between">
+                <span className="block text-lg font-semibold">{item.name}</span>
+                <span className="block font-semibold">${item.price}</span>
+              </div>
+              <p className="text-sm mt-2">{item.description}</p>
             </div>
-            <p className="text-sm mt-2">{item.description}</p>
-          </div>
-          <div className="mt-4">
-            <Button
-              className="bg-white border-2 border-black text-black px-4 py-1 text-lg rounded-lg font-medium hover:bg-black hover:text-white transition"
-              onClick={() => handleClick(item)}
-            >
-              Add to Cart
-            </Button>
-          </div>
-        </Card>
-      ))}
+            <div className="mt-4">
+              <Button
+                className="bg-white border-2 border-black text-black px-4 py-1 text-lg rounded-lg font-medium hover:bg-black hover:text-white transition"
+                onClick={() => handleClick(item)}
+              >
+                Add to Cart
+              </Button>
+            </div>
+          </Card>
+        ))
+      )}
     </div>
   );
 }
