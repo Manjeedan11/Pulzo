84dcba36e8147f94a299df4e57ea72c643358fbe
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-01-20 21:05:30 +0530

    implemented a conditional rendering if the cart is empty

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/CartItem.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/CartItem.jsx
@@ -12,35 +12,41 @@ function CartItem(props) {
 
   return (
     <div>
-      {cartItems.map((item) => (
-        <div
-          key={item.product._id}
-          className="flex items-center justify-between border-b border-gray-200 py-4"
-        >
-          <div className="flex items-center">
-            <img
-              src={item.product.image}
-              alt={item.product.name}
-              className="w-16 h-16 object-cover mr-4 rounded-lg"
-            />
-            <div>
-              <h3 className="text-lg font-semibold">{item.product.name}</h3>
-              <p className="text-gray-600">${item.product.price}</p>
+      {cartItems.length === 0 ? (
+        <p className="text-center w-full text-xl font-semibold">
+          Cart is empty
+        </p>
+      ) : (
+        cartItems.map((item) => (
+          <div
+            key={item.product._id}
+            className="flex items-center justify-between border-b border-gray-200 py-4"
+          >
+            <div className="flex items-center">
+              <img
+                src={item.product.image}
+                alt={item.product.name}
+                className="w-16 h-16 object-cover mr-4 rounded-lg"
+              />
+              <div>
+                <h3 className="text-lg font-semibold">{item.product.name}</h3>
+                <p className="text-gray-600">${item.product.price}</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <span className="px-4 py-1 bg-gray-100 rounded-full">
+                {item.quantity}
+              </span>
+              <button
+                className="ml-4 text-red-500 hover:text-red-700 rounded-full px-3 py-1 bg-red-100 hover:bg-red-200 transition duration-300"
+                onClick={() => handleRemove(item.product._id)}
+              >
+                <Trash2 className="text-red-500" />
+              </button>
             </div>
           </div>
-          <div className="flex items-center">
-            <span className="px-4 py-1 bg-gray-100 rounded-full">
-              {item.quantity}
-            </span>
-            <button
-              className="ml-4 text-red-500 hover:text-red-700 rounded-full px-3 py-1 bg-red-100 hover:bg-red-200 transition duration-300"
-              onClick={() => handleRemove(item.product._id)}
-            >
-              <Trash2 className="text-red-500" />
-            </button>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 }
