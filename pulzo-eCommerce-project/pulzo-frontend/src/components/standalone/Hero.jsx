25348779aa566bc278a66d26642be61a86f003ea
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-01-21 13:54:02 +0530

    refactored the code with some changes in stylings

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/Hero.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/Hero.jsx
@@ -16,7 +16,7 @@ function Hero() {
 
   return (
     <>
-      <section className="py-8 px-4 xl:px-16">
+      <section className="py-8 px-4 xl:px-16 mt-5">
         <div className="grid grid-cols-2 rounded-md min-h-[60vh] bg-[#f4f8f9]">
           <div className="flex flex-col justify-center p-8 md:p-16 gap-y-4">
             <span className="inline-block rounded-full px-2 py-1 text-xs w-fit bg-[#febc26]">
