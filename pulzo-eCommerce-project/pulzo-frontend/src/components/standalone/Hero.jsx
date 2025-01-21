c311ac7ef6c2211c957d7d77914dd4a2d9b11ec5
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-01-21 12:11:02 +0530

    implemented scrollView effect from Hero -> Products in HomePage using useRef hook

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/Hero.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/Hero.jsx
@@ -1,37 +1,57 @@
 import { Button } from "@/components/ui/button";
 import heroBanner from "@/assets/hero/hero.png";
+import Products from "./Products";
+import { useRef } from "react";
 
 function Hero() {
+  const productRef = useRef(null);
+
+  const scrollToProduct = () => {
+    productRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
   return (
-    <section className="py-8 px-4 xl:px-16">
-      <div className="grid grid-cols-2 rounded-md min-h-[60vh] bg-[#f4f8f9]">
-        <div className="flex flex-col justify-center p-8 md:p-16 gap-y-4">
-          <span className="inline-block rounded-full px-2 py-1 text-xs w-fit bg-[#febc26]">
-            WEEKLY DISCOUNT
-          </span>
-          <h1 className="text-[3.75rem] font-semibold leading-none">
-            Premium Product Online Shop
-          </h1>
-          <p>
-            Your Future of Shopping. Explore, Compare, and Buy the Latest
-            Electronics
-          </p>
-          <Button
-            className="w-fit px-4 py-2 text-white font-medium bg-black rounded-md"
-            asChild
-          >
-            <p>Shop now</p>
-          </Button>
+    <>
+      <section className="py-8 px-4 xl:px-16">
+        <div className="grid grid-cols-2 rounded-md min-h-[60vh] bg-[#f4f8f9]">
+          <div className="flex flex-col justify-center p-8 md:p-16 gap-y-4">
+            <span className="inline-block rounded-full px-2 py-1 text-xs w-fit bg-[#febc26]">
+              WEEKLY DISCOUNT
+            </span>
+            <h1 className="text-[3.75rem] font-semibold leading-none">
+              Premium Product Online Shop
+            </h1>
+            <p>
+              Your Future of Shopping. Explore, Compare, and Buy the Latest
+              Electronics
+            </p>
+            <Button
+              className="w-fit px-4 py-2 text-white font-medium bg-black rounded-md"
+              onClick={scrollToProduct}
+              asChild
+            >
+              <p>Shop now</p>
+            </Button>
+          </div>
+          <div className="relative">
+            <img
+              className="w-full h-full object-cover"
+              src={heroBanner}
+              alt="Banner"
+            />
+          </div>
         </div>
-        <div className="relative">
-          <img
-            className="w-full h-full object-cover"
-            src={heroBanner}
-            alt="Banner"
-          />
+      </section>
+
+      <section className="py-10">
+        <div ref={productRef} className="container mx-auto">
+          <Products />
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
