aeb9dbef33a6ca1be0833f0b0ec1fe89c2f0b796
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-01-23 22:29:09 +0530

    implemented a basic useLocation hook logic for search feature

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/SearchBar.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/SearchBar.jsx
@@ -1,8 +1,16 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react";
+import { useGetProductsQuery } from "@/lib/api";
+import { useLocation } from "react-router";
 
 function SearchBar() {
+  const { search } = useLocation();
+  const queryParams = new URLSearchParams(search);
+  const category = queryParams.get("product") || "";
+
+  const { data: products, isLoading, isError } = useGetProductsQuery();
+
   return (
     <div className="relative">
       <Input className="w-100 pr-12 rounded-full" placeholder="Search" />
