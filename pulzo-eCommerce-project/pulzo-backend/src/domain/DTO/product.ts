806685fe56c60bbec09f8bd87d3d1b69e8562b61
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-02-15 21:08:52 +0530

    refactored the zod validation logic for new fields

--- pulzo-eCommerce-project/pulzo-backend/src/domain/DTO/product.ts
+++ pulzo-eCommerce-project/pulzo-backend/src/domain/DTO/product.ts
@@ -3,13 +3,14 @@ import { z } from "zod";
 const ObjectIdRegex = /^[a-fA-F0-9]{24}$/;
 
 export const ProductDTO = z.object({
-  categoryId: z
-    .string()
-    .regex(ObjectIdRegex, {
-      message: "Invalid ObjectId format for categoryId",
-    }),
+  categoryId: z.string().regex(ObjectIdRegex, {
+    message: "Invalid ObjectId format for categoryId",
+  }),
   image: z.string(),
   name: z.string(),
   price: z.string(),
+  synopsis: z.string(),
   description: z.string(),
+  ratings: z.number().min(1).max(5),
+  keyFeatures: z.array(z.string()),
 });
