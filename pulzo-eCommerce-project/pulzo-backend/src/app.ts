13c85dc4c2aec595dea35bb2ecbb4957c0534a58
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-02-22 10:53:55 +0530

    implemented DTO to validate the order and shippingAddress fields

--- pulzo-eCommerce-project/pulzo-backend/src/app.ts
+++ pulzo-eCommerce-project/pulzo-backend/src/app.ts
@@ -6,10 +6,12 @@ import { userRouter } from "./api/user";
 import globalErrorHandlingMiddleware from "./api/middleware/global-error-handling-middleware";
 import { connectDB } from "./infrastructure/db";
 import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
+app.use(clerkMiddleware());
 
 app.use("/api/products", productRouter);
 app.use("/api/categories", categoriesRouter);
