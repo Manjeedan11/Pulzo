a2427d19ed9636dde18c9847b94a7d40a4020a39
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-02-28 22:09:03 +0530

    added magicUI dependencies and implemented useEffect hook logic to manage the render of text-animate in the input's placeholder text

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/SearchBar.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/SearchBar.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react";
+import { TextAnimate } from "../magicui/text-animate";
 
 function SearchBar() {
   const [query, setQuery] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
   const navigate = useNavigate();
+  const placeholders = [
+    "Search for the latest gadgets – Try ‘Echo Dot’",
+    "Search by category – Laptops, Mobiles & more",
+    "Search for the best tech deals – Limited-time offers !",
+  ];
+  const [placeholderIndex, setPlaceholderIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPlaceholderIndex((prev) => (prev + 1) % placeholders.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
   const handleSearch = () => {
     if (query.trim()) {
@@ -22,13 +37,29 @@ function SearchBar() {
 
   return (
     <div className="relative w-full">
+      {!query && !isFocused && (
+        <div className="absolute left-5 top-1/2 -translate-y-1/2 pointer-events-none">
+          <TextAnimate
+            variant="fadeIn"
+            duration={0.8}
+            delay={0.2}
+            className="text-gray-400 text-xs"
+            key={placeholderIndex}
+          >
+            {placeholders[placeholderIndex]}
+          </TextAnimate>
+        </div>
+      )}
+
       <Input
-        className="w-[409px] pr-20 rounded-full"
-        placeholder="Search"
+        className="w-[409px] pr-20 pl-[22px] rounded-full"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={handleKeyDown}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
+
       <Button
         className="absolute right-0 top-0 h-full bg-gray-400 rounded-full"
         onClick={handleSearch}
