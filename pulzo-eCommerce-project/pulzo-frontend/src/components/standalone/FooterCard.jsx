6b711a6fbf041817fb5481c1c7dd35395f9dcc61
Manjeedan <shakthivelmanjeedan11@gmail.com>
2025-06-03 20:57:52 +0530

    style : alter site name in footer component

--- pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/FooterCard.jsx
+++ pulzo-eCommerce-project/pulzo-frontend/src/components/standalone/FooterCard.jsx
@@ -11,7 +11,7 @@ function FooterCard() {
         <div className="grid gap-8 lg:grid-cols-[2fr,1fr,1fr,1fr,1fr]">
           <div className="space-y-4">
             <Link href="/" className="inline-flex items-center space-x-2">
-              <p className=" text-3xl font-bold">ρυℓzσ</p>
+              <p className=" text-3xl font-bold">tECHnOcaRT</p>
             </Link>
             <p className="max-w-sm font-poppins text-black ">
               Discover over 7,000 premium electronic gadgets for all your tech
